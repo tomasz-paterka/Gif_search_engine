b22b012a526d3fcba71eb14b7feb978ec4aab8c1
Tomasz Paterka <tomekpaterka@gmail.com>
2018-03-28 23:25:07 +0200

    Add scripts

---
+++ components/App.js
@@ -0,0 +1,60 @@
+App = React.createClass({
+	getInitialState() {
+		return {
+			loading: false,
+			searchingText: '',
+			gif: {}
+		};
+	},
+	
+	handleSearch: function(searchingText) {
+		this.setState({
+			loading: true	
+		});
+		this.getGif(searchingText, function(gif) {
+			this.setState({
+				loading: false,
+				gif: gif,
+				searchingText: searchingText
+			});
+		}.bind(this));
+	},
+	
+	getGif: function(searchingText, callback) {
+		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url);
+		xhr.onload = function() {
+			if (xhr.status === 200) {
+				var data = JSON.parse(xhr.responseText).data;
+				var gif = {
+					url: data.fixed_width_downsampled_url,
+					sourceUrl: data.url
+				};
+				callback(gif);
+			}
+		};
+		xhr.send();
+	},
+	
+	render: function() {
+		var styles = {
+			margin: '0 auto',
+			textAlign: 'center',
+			width: '90%'
+		};
+		
+		return (
+			<div style={styles}>
+				<h1>GIF search engine!</h1>
+				<p>Find gif at <a href='http://giphy.com'>giphy</a>. Press enter to get more gifs</p>
+				<Search onSearch={this.handleSearch}/>
+				<Gif 
+					loading={this.state.loading}
+					url={this.state.gif.url}
+					sourceUrl={this.state.gif.sourceUrl}
+				/>
+			</div>
+		);
+	}
+});
