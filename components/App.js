84e985f35b09e483275325b3ad7479dd2af55692
Tomasz Paterka <tomekpaterka@gmail.com>
2018-03-29 21:14:07 +0200

    Add new variables to getGif, and ReactDOM.render to script.js

--- components/App.js
+++ components/App.js
@@ -21,6 +21,8 @@ App = React.createClass({
 	},
 	
 	getGif: function(searchingText, callback) {
+		var GIPHY_API_URL = 'https://api.giphy.com';
+		var GIPHY_PUB_KEY = 'QsbkD07YukG4zXJ7BrkydMJTau3EHjul';
 		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', url);
