ab99936655e8fa80710c6d484679dd2ce40f036f
Tomasz Paterka <tomekpaterka@gmail.com>
2018-06-05 20:04:37 +0200

    add styles and png

--- components/App.js
+++ components/App.js
@@ -49,14 +49,8 @@ App = React.createClass({
 	},
 	
 	render: function() {
-		var styles = {
-			margin: '0 auto',
-			textAlign: 'center',
-			width: '90%'
-		};
-		
 		return (
-			<div style={styles}>
+			<div className="app">
 				<h1>GIF search engine!</h1>
 				<p>Find gif at <a href='http://giphy.com'>giphy</a>. Press enter to get more gifs</p>
 				<Search onSearch={this.handleSearch}/>
